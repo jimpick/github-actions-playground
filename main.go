797b5ac0d21e0740a98604516b0630124d8b021b
Jim Pick <jim@jimpick.com>
2021-11-17 14:54:03 -0800

    Try example from paketo examples

--- main.go
+++ main.go
@@ -1,34 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+const INDEX = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Powered By Paketo Buildpacks</title>
+  </head>
+  <body>
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
+  </body>
+</html>`
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, INDEX)
+	})
 
-	router.Run("localhost:8085")
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
